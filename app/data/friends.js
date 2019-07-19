3b034b48da7b3f1994095d63bfff804d9564bba4
Ellen N Evans <ellennewellevans@MacPro-EEvans.local>
2019-07-19 11:06:33 -0400

    added routes

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friendsArray = [
+    {name: "Ellen Evans",
+    photo: "https://s-media-cache-ak0.pinimg.com/originals/a9/09/b4/a909b465a163e1c0b895b6e8843d99bc.jpg",
+    scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]}
+];
+
+module.exports = friendsArray;
